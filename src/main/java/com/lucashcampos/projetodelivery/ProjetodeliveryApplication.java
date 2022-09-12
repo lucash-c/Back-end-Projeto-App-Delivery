9afb1ed8d071e09f0882d336b90e4f2081795b15
Lucas Campos <lucash_c@yahoo.com.br>
2022-09-12 15:51:35 -0300

    first commit

---
+++ src/main/java/com/lucashcampos/projetodelivery/ProjetodeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.lucashcampos.projetodelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetodeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetodeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucashcampos/projetodelivery/ProjetodeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucashcampos.projetodelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetodeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
