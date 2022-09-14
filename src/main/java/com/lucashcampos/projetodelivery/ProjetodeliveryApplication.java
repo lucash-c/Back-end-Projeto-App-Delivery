90e1d6c8436721dacc31100ea2fc39b52930652a
Lucas Campos <lucash_c@yahoo.com.br>
2022-09-14 12:11:59 -0300

    Criando operação de instanciação

--- src/main/java/com/lucashcampos/projetodelivery/ProjetodeliveryApplication.java
+++ src/main/java/com/lucashcampos/projetodelivery/ProjetodeliveryApplication.java
@@ -1,13 +1,32 @@
 package com.lucashcampos.projetodelivery;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.lucashcampos.projetodelivery.domain.Categoria;
+import com.lucashcampos.projetodelivery.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class ProjetodeliveryApplication {
+public class ProjetodeliveryApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetodeliveryApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1 , cat2));
+		
 	}
 
 }
