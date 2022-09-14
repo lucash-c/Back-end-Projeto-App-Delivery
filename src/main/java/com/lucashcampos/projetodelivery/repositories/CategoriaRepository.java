1f3326ef993acb1ab1d1346f55a17d04e7c360a1
Lucas Campos <lucash_c@yahoo.com.br>
2022-09-14 12:01:40 -0300

    Criando repository e service para categoria

---
+++ src/main/java/com/lucashcampos/projetodelivery/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.lucashcampos.projetodelivery.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lucashcampos.projetodelivery.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
