ae32e717e3f90ef84dba37fc165dfe9202d21aa2
Lucas Campos <lucash_c@yahoo.com.br>
2022-09-23 19:36:32 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/lucashcampos/projetodelivery/services/CategoriaService.java
+++ src/main/java/com/lucashcampos/projetodelivery/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.lucashcampos.projetodelivery.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.lucashcampos.projetodelivery.domain.Categoria;
 import com.lucashcampos.projetodelivery.repositories.CategoriaRepository;
+import com.lucashcampos.projetodelivery.services.exceptions.DataIntegrityException;
 import com.lucashcampos.projetodelivery.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,5 +31,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos!");
+		}
+
 	}
 }
