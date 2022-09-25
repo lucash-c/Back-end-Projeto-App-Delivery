d432024f01638401157508abe71c193fb3b95c4d
Lucas Campos <lucash_c@yahoo.com.br>
2022-09-25 00:37:02 -0300

    Inserindo um novo cliente com POST

--- src/main/java/com/lucashcampos/projetodelivery/resources/ClienteResource.java
+++ src/main/java/com/lucashcampos/projetodelivery/resources/ClienteResource.java
@@ -19,6 +19,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.lucashcampos.projetodelivery.domain.Cliente;
 import com.lucashcampos.projetodelivery.dto.ClienteDTO;
+import com.lucashcampos.projetodelivery.dto.ClienteNewDTO;
 import com.lucashcampos.projetodelivery.services.ClienteService;
 
 @RestController
@@ -42,7 +43,7 @@ public class ClienteResource {
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteDTO objDTO) {
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDTO) {
 		Cliente obj = service.fromDTO(objDTO);
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
--- src/main/java/com/lucashcampos/projetodelivery/services/ClienteService.java
+++ src/main/java/com/lucashcampos/projetodelivery/services/ClienteService.java
@@ -9,10 +9,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.lucashcampos.projetodelivery.domain.Cidade;
 import com.lucashcampos.projetodelivery.domain.Cliente;
+import com.lucashcampos.projetodelivery.domain.Endereco;
+import com.lucashcampos.projetodelivery.domain.enums.TipoCliente;
 import com.lucashcampos.projetodelivery.dto.ClienteDTO;
+import com.lucashcampos.projetodelivery.dto.ClienteNewDTO;
 import com.lucashcampos.projetodelivery.repositories.ClienteRepository;
+import com.lucashcampos.projetodelivery.repositories.EnderecoRepository;
 import com.lucashcampos.projetodelivery.services.exceptions.DataIntegrityException;
 import com.lucashcampos.projetodelivery.services.exceptions.ObjectNotFoundException;
 
@@ -21,6 +27,9 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository repo;
+
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public Cliente find(Integer id) {
 
@@ -33,9 +42,12 @@ public class ClienteService {
 		return repo.findAll();
 	}
 
+	@Transactional
 	public Cliente insert(Cliente obj) {
 		obj.setId(null);
-		return repo.save(obj);
+		obj=repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
 	}
 
 	public Cliente update(Cliente obj) {
@@ -61,6 +73,20 @@ public class ClienteService {
 
 	public Cliente fromDTO(ClienteDTO objDTO) {
 		return new Cliente(objDTO.getId(), null, objDTO.getNome(), objDTO.getEmail(), null);
+	}
+
+	public Cliente fromDTO(ClienteNewDTO objDTO) {
+		Cliente cli = new Cliente(null, objDTO.getCpf_cnpj(), objDTO.getNome(), objDTO.getEmail(),
+				TipoCliente.toEnum(objDTO.getTipo()));
+		Cidade cid = new Cidade(objDTO.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDTO.getLogradouro(), objDTO.getNumero(), objDTO.getComplemento(),
+				objDTO.getBairro(), objDTO.getCep(), cid, cli);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDTO.getTelefone1());
+		if (objDTO.getTelefone2() != null) {
+			cli.getTelefones().add(objDTO.getTelefone2());
+		}
+		return cli;
 	}
 
 	private void updateData(Cliente newObj, Cliente obj) {
