72cc8524ed3650c42aa47bb4b25c1e27fc68a884
Lucas Campos <lucash_c@yahoo.com.br>
2022-10-07 19:26:39 -0300

    Implementando autorizacao

--- src/main/java/com/lucashcampos/projetodelivery/security/JWTAuthenticationFilter.java
+++ src/main/java/com/lucashcampos/projetodelivery/security/JWTAuthenticationFilter.java
@@ -62,14 +62,14 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 		@Override
 		public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
 				AuthenticationException exception) throws IOException, ServletException {
-			response.setStatus(403);
+			response.setStatus(401);
 			response.setContentType("application/json");
 			response.getWriter().append(json());
 		}
 
 		private String json() {
 			long date = new Date().getTime();
-			return "{\"timestamp\": " + date + ", " + "\"status\": 403, " + "\"error\": \"Não autorizado\", "
+			return "{\"timestamp\": " + date + ", " + "\"status\": 401, " + "\"error\": \"Não autorizado\", "
 					+ "\"message\": \"Email ou senha inválidos\", " + "\"path\": \"/login\"}";
 		}
 	}
