fd134c6468fa5922e6e320227deaa703fbec32a7
Lucas Campos <lucash_c@yahoo.com.br>
2022-10-07 20:12:43 -0300

    Implementado service que retorna o usuario logado

---
+++ src/main/java/com/lucashcampos/projetodelivery/services/UserService.java
@@ -0,0 +1,15 @@
+package com.lucashcampos.projetodelivery.services;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.lucashcampos.projetodelivery.security.UserSS;
+
+public class UserService {
+	public static UserSS authenticated() {
+		try {
+			return (UserSS) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		} catch (Exception e) {
+			return null;
+		}
+	}
+}
